8383c4db2622568fae48ac51cd829faac6d17831
Jiazhe Chen <chenjiazhe@outlook.com>
2022-09-12 19:04:42 -0400

    Build a signed APK.

--- app/src/main/java/edu/northeastern/numad22fa_jiazhechen/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_jiazhechen/MainActivity.java
@@ -4,23 +4,31 @@ package edu.northeastern.numad22fa_jiazhechen;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView aboutmeText;
-
-    public void proc() {
-        Button aboutmeButton = (Button) findViewById(R.id.aboutmeButton);
-        aboutmeText = (TextView) findViewById(R.id.aboutmeText);
-
-        aboutmeButton.setOnClickListener(view -> aboutmeText.setText(R.string.aboutmeText));
-    }
+    Button aboutmeButton;
+    TextView aboutmeText;
+    String hello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        proc();
+
+        aboutmeButton = new Button(this);
+        aboutmeText =new TextView(this);
+
+        aboutmeButton = (Button)findViewById(R.id.aboutmeButton);
+        aboutmeText=(TextView)findViewById(R.id.aboutmeText);
+
+        hello = "Jiazhe Chen, chen.jiazhe@northeastern.edu";
+
+        aboutmeButton.setOnClickListener((View.OnClickListener) this);
+    }
+    public void onClick(View view){
+        aboutmeText.setText(hello);
     }
 }
