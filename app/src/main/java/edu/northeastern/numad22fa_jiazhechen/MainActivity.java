d05509e0c8f432f02c1da1a674df8d6b2931aea8
Jiazhe Chen <chenjiazhe@outlook.com>
2022-09-12 15:10:54 -0400

    Initial commit

---
+++ app/src/main/java/edu/northeastern/numad22fa_jiazhechen/MainActivity.java
@@ -0,0 +1,18 @@
+package edu.northeastern.numad22fa_jiazhechen;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.v(TAG, "onCreate()");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
