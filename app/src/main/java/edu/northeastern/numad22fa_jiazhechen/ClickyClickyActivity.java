f119c666ba07062174ca807c4af999d88679e23d
Jiazhe Chen <chenjiazhe@outlook.com>
2022-09-25 20:17:51 -0400

    Set event for buttons on the new activity.

--- app/src/main/java/edu/northeastern/numad22fa_jiazhechen/ClickyClickyActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_jiazhechen/ClickyClickyActivity.java
@@ -1,36 +1,34 @@
 package edu.northeastern.numad22fa_jiazhechen;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class ClickyClickyActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView pressTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clicky_clicky);
-        TextView pressTextView = findViewById(R.id.pressTextView);
+        pressTextView = findViewById(R.id.pressTextView);
     }
 
     public void onClick(View view) {
         int theId = view.getId();
         if (theId == R.id.aButton) {
-
+            pressTextView.setText(R.string.press_a);
         } else if (theId == R.id.bButton) {
-
+            pressTextView.setText(R.string.press_b);
         } else if (theId == R.id.cButton) {
-
+            pressTextView.setText(R.string.press_c);
         } else if (theId == R.id.dButton) {
-
+            pressTextView.setText(R.string.press_d);
         } else if (theId == R.id.eButton) {
-
+            pressTextView.setText(R.string.press_e);
         } else if (theId == R.id.fButton) {
-
+            pressTextView.setText(R.string.press_f);
         }
     }
 }
--- app/src/main/java/edu/northeastern/numad22fa_jiazhechen/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_jiazhechen/MainActivity.java
@@ -1,12 +1,9 @@
 package edu.northeastern.numad22fa_jiazhechen;
 
-
-
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -15,14 +12,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView helloTextView = findViewById(R.id.hello);
     }
 
     public void onClick(View view) {
         int theId = view.getId();
         if (theId == R.id.aboutmeButton) {
             Toast.makeText(getApplicationContext(),
-                    "Jiazhe Chen\nchen.jiazhe@northeastern.edu",
+                    R.string.aboutmeText,
                     Toast.LENGTH_SHORT).show();
         } else if (theId == R.id.clickyClickyButton) {
             Intent clickyIntent = new Intent(this, ClickyClickyActivity.class);
