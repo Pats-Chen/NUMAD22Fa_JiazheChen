6b072347b9780c6368b5ccf8f34d3281986d1431
Jiazhe Chen <chenjiazhe@outlook.com>
2022-10-03 22:10:28 -0400

    Implemented a snackbar for updating the list in recycler view.

--- app/src/main/java/edu/northeastern/numad22fa_jiazhechen/LinkCollectorActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_jiazhechen/LinkCollectorActivity.java
@@ -8,10 +8,12 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 
-public class LinkCollectorActivity extends AppCompatActivity {
+public class LinkCollectorActivity extends AppCompatActivity implements LinkInputFragment.LinkDialogListener {
     RecyclerView recyclerView;
     FloatingActionButton fab;
 
@@ -30,13 +32,14 @@ public class LinkCollectorActivity extends AppCompatActivity {
 
 //            Intent intent = new Intent(LinkCollectorActivity.this, AddActivity.class);
 //            startActivity(intent)
+//        linkName.add("Google");
+//        linkString.add("www.google.com");
+//        linkName.add("Facebook");
+//        linkString.add("www.facebook.com");
 
         linkName = new ArrayList<>();
         linkString = new ArrayList<>();
-        linkName.add("Google");
-        linkString.add("www.google.com");
-        linkName.add("Facebook");
-        linkString.add("www.facebook.com");
+
 
 //        storeDataInArrays();
 
@@ -48,5 +51,18 @@ public class LinkCollectorActivity extends AppCompatActivity {
     private void openDialog() {
         LinkInputFragment linkInputFragment = new LinkInputFragment();
         linkInputFragment.show(getSupportFragmentManager(), "USER INPUT FRAGMENT");
+    }
+
+    @Override
+    public void sendInfo(String urlName, String urlString) {
+        linkName.add(urlName);
+        linkString.add(urlString);
+        showSnackbar();
+    }
+
+    public void showSnackbar() {
+        Snackbar successSnackbar = Snackbar.make(findViewById(R.id.linkCollectorLayout), "The link was successfully created!", Snackbar.LENGTH_SHORT);
+        successSnackbar.show();
+
     }
 }
--- app/src/main/java/edu/northeastern/numad22fa_jiazhechen/LinkInputFragment.java
+++ app/src/main/java/edu/northeastern/numad22fa_jiazhechen/LinkInputFragment.java
@@ -2,23 +2,23 @@ package edu.northeastern.numad22fa_jiazhechen;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDialogFragment;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
-
-import org.w3c.dom.Text;
 
 public class LinkInputFragment extends AppCompatDialogFragment {
     private TextInputEditText linkName;
     private TextInputEditText linkString;
+    private LinkDialogListener listener;
 
     @NonNull
     @Override
@@ -29,7 +29,9 @@ public class LinkInputFragment extends AppCompatDialogFragment {
         dialogBuilder.setView(view);
         dialogBuilder.setTitle("URL Input")
                 .setPositiveButton("SUBMIT", (dialogInterface, i) -> {
-
+                    String urlName = linkName.getText().toString();
+                    String urlString = linkString.getText().toString();
+                    listener.sendInfo(urlName, urlString);
                 });
 
         linkName = view.findViewById(R.id.linkNameInput);
@@ -39,5 +41,16 @@ public class LinkInputFragment extends AppCompatDialogFragment {
 //        final EditText linkString = new EditText(getContext());
 
         return dialogBuilder.create();
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        listener = (LinkDialogListener) context;
+    }
+
+    public interface LinkDialogListener {
+        void sendInfo(String urlName, String urlString);
     }
 }
